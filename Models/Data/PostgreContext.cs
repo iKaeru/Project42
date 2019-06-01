8c1995e5bb5672df3838991ed0449804942325a9
Fedor Okladnikov <fedya.ok96@gmail.com>
2019-06-01 12:55:26 +0700

    Trying to setup DB

--- Models/Data/PostgreContext.cs
+++ Models/Data/PostgreContext.cs
@@ -8,15 +8,23 @@ namespace Models.Data
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql(
-                "Host=localhost;" +
-                "Port=5432;" +
-                "Database=cards;" +
-                "Username=postgres;" +
-                "Password=12345");
+            //optionsBuilder.UseNpgsql(  //Local DB
+            //    "Host=localhost;" +
+            //    "Port=5432;" +
+            //    "Database=cards;" +
+            //    "Username=postgres;" +
+            //    "Password=1q2w3e");
+
+            optionsBuilder.UseNpgsql(  //Cloud DB
+                "Host=rc1b-xfcvuyuupeyfsv8y.mdb.yandexcloud.net;" +
+                "Port=6432;" +
+                "Database=db1;" +
+                "SSL Mode = Prefer;" +
+                "Username=user1;" +
+                "Password=1q2w3e4r");
+
         }
 
-        
         public DbSet<User.User> Users { get; set; }
         public DbSet<CardItem.CardItem> Cards { get; set; }
         public DbSet<Models.CardsCollection.CardsCollection> Collections { get; set; }
