1507addb107d3bd0d0dd843bde51a00eba829a4e
Fedor Okladnikov <fedya.ok96@gmail.com>
2019-06-02 19:49:56 +0700

    increased security a bit

--- MemoryCardsAPI/Program.cs
+++ MemoryCardsAPI/Program.cs
@@ -10,29 +10,18 @@ namespace MemoryCardsAPI
         static void Main(string[] args)
         {
             var config = new ConfigurationBuilder()
-             //   .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: true)
-            //    .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-            //    .AddCommandLine(args)
                 .Build();
 
             var host = new WebHostBuilder()
-                .UseUrls("http://*:5001")
+                .UseUrls("http://*:5000")
                 .UseEnvironment("Development")
                 .UseConfiguration(config)
                 .UseKestrel()
-          //      .UseContentRoot(Directory.GetCurrentDirectory())
-           //     .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
             host.Run();
-            
-//            WebHost
-//                .CreateDefaultBuilder(args)
-//                .UseStartup<Startup>()
-//                .Build()
-//                .Run();
         }
     }
 }
